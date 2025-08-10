b97536d2d579bc1eaa62da013c2cffe06ad610e6
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2025-08-11 01:39:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,10 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
   ),
-  title: '0xAquaWolf Portfolio',
+  title: 'Nikolay Site',
 
   description:
-    'AquaWolf is a Senior Full-Stack Software Engineer and indie hacker with a proven track record in building impactful, consumer-facing products that have generated over $28M in Web3 projects and $30K MRR in e-commerce. Specializing in rapid prototyping, full-stack development, UI/UX design, and emerging technologies like AI and blockchain, AquaWolf is passionately building innovative solutions and upskilling in Web3, AI, and Machine Learning while releasing quality apps to the public.',
+    'Nikolay Site is a portfolio website',
   icons: {
     icon: [
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
